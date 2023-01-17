938f055d752398f7d6c145edc4e59149d21a3eea
santbetv <santiago-betancur@hotmail.com>
2023-01-17 00:24:38 -0500

    first commit files

---
+++ src/main/java/com/hexagonal/apirest/ProyectoApirestHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApirestHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApirestHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/apirest/ProyectoApirestHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApirestHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
