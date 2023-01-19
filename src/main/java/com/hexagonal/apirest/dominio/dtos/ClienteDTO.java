a37bcaa35f43ca26e2209ef6eb4ac0d06ed6ab7b
santbetv <santiago-betancur@hotmail.com>
2023-01-18 23:50:24 -0500

    Se adiciona clase cliente en Dominio y DTO

---
+++ src/main/java/com/hexagonal/apirest/dominio/dtos/ClienteDTO.java
@@ -0,0 +1,15 @@
+package com.hexagonal.apirest.dominio.dtos;
+
+import lombok.Getter;
+
+@Getter
+public class ClienteDTO {
+
+	private String nombre;
+	private String apellido;
+	private String identificacion;
+	private String telefono;
+	private String email;
+
+
+}
